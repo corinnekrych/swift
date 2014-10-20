2f02049be41ee2b1e1c5dc44a914fa6386d9db50
corinne krych <corinnekrych@gmail.com>
2014-10-20 16:24:56 +0200

    Agenda and structure slides

--- Extension.playground/section-1.swift
+++
@@ -1,1 +0,0 @@
-// http://www.jayway.com/2014/06/26/from-objective-c-to-swift-swift-goodies/
--- Introduction.playground/section-1.swift
+++ Introduction.playground/section-1.swift
@@ -3,5 +3,3 @@
 import UIKit
 
 var str = "Hello, playground"
-
-//https://medium.com/swift-programming/the-swift-compiler-and-the-standard-library-237e4dc73107
