3a3e151d115c1fd25f1fbd82967eca20ef47042e
corinne krych <corinnekrych@gmail.com>
2014-10-21 10:50:24 +0200

    Objc interop

---
+++ 3.2.Interoperability.playground/section-2.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+var objcString: NSString = "My ObjC String"
+
+import UIKit
+ 
+let testLabel = UILabel(frame: CGRectMake(0, 0, 120, 40))
+testLabel.text = "Hello, Swift!"
+testLabel.backgroundColor = UIColor(red: 0.9, green: 0.2, blue: 0.2, alpha: 1.0)
+testLabel.textAlignment = NSTextAlignment.Center
--- 3.2.Objectve-C_Interoperability.playground/section-1.swift
+++
@@ -1,3 +0,0 @@
-// http://sketchytech.blogspot.fr/2014/09/swift-basic-step-by-step-use-of-swift.html
-// @objc
-// http://www.codingricky.com/swift-and-objective-c-a-happy-couple/
