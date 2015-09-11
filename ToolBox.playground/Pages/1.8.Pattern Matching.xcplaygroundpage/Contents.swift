de92fbbbb40a2fac239306af6ff48a169fcc1756
corinne krych <corinnekrych@gmail.com>
2015-09-11 12:03:59 +0200

    add pattern matching section

--- ToolBox.playground/Pages/1.8.Pattern Matching.xcplaygroundpage/Contents.swift
+++ ToolBox.playground/Pages/1.8.Pattern Matching.xcplaygroundpage/Contents.swift
@@ -1,7 +1,58 @@
 //: [Previous](@previous)
 
-import Foundation
+//: ## Working with enum
+//: examples taken from [Match Me if you can: Swift Pattern Matching in Detail](http://appventure.me/2015/08/20/swift-pattern-matching-in-detail/) blog post
 
-var str = "Hello, playground"
+enum TraderType {
+    case SingleGuy
+    case Company
+}
+
+enum Trades {
+    case Buy(stock: String, amount: Int, stockPrice: Float, type: TraderType)
+    case Sell(stock: String, amount: Int, stockPrice: Float, type: TraderType)
+}
+
+let aTrade = Trades.Sell(stock: "GOOG", amount: 100, stockPrice: 666.0, type: TraderType.Company)
+
+
+func processSlow(stock: String, _ amount: Int, _ fee: Float) { print("slow") }
+func processFast(stock: String, _ amount: Int, _ fee: Float) { print("fast") }
+
+switch aTrade {
+case let .Buy(stock, amount, _, TraderType.SingleGuy):
+    processSlow(stock, amount, 5.0)
+case let .Sell(stock, amount, _, TraderType.SingleGuy):
+    processSlow(stock, -1 * amount, 5.0)
+case let .Buy(stock, amount, _, TraderType.Company):
+    processFast(stock, amount, 2.0)
+case let .Sell(stock, amount, _, TraderType.Company):
+    processFast(stock, -1 * amount, 2.0)
+}
+
+
+//: Adding guard
+
+let aTrade2 = Trades.Buy(stock: "GOOG", amount: 1000, stockPrice: 666.0, type: TraderType.SingleGuy)
+
+switch aTrade2 {
+case let .Buy(stock, amount, _, TraderType.SingleGuy):
+    processSlow(stock, amount, 5.0)
+case let .Sell(stock, amount, price, TraderType.SingleGuy)
+    where price*Float(amount) > 1000000:
+    processFast(stock, -1 * amount, 5.0)
+case let .Sell(stock, amount, _, TraderType.SingleGuy):
+    processSlow(stock, -1 * amount, 5.0)
+case let .Buy(stock, amount, price, TraderType.Company)
+    where price*Float(amount) < 1000:
+    processSlow(stock, amount, 2.0)
+case let .Buy(stock, amount, _, TraderType.Company):
+    processFast(stock, amount, 2.0)
+case let .Sell(stock, amount, _, TraderType.Company):
+    processFast(stock, -1 * amount, 2.0)
+}
+
+//: Note that a ```switch``` statement should be complete. The compiler will remind you if not asking you to add a default case.
+
 
 //: [Next](@next)
